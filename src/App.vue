569c33957666bb2a15fcd2a58bbb7aa760674334
z4bulon <z8bulon@gmail.com>
2024-06-18 00:02:39 +0500

    Created a form component, changed the App.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SignUpForm/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SignUpForm from "@/components/SignUpForm.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignUpForm
   }
 }
 </script>
@@ -22,5 +20,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  margin: 0;
+  background: #eee;
 }
 </style>
